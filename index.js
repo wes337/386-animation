61ddd758aad239339a56a2d08f60fac7893f6aa1
wes337 <wesmoses@gmail.com>
2021-05-16 08:40:35 +0300

    exports and named fn

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-export default function init386(options = {}) {
+exports.init386 = function (options = {}) {
   var character = { height: 20, width: 12.4 };
-  const loading = () => {
+  function loading() {
     if (options.fastLoad) {
-      document.body.style.visibility = 'visible';
+      document.body.style.visibility = "visible";
       return;
     }
 
-    var
-      onePass = options.onePass,
-      speedFactor = 1 / (options.speedFactor || 1) * 165000,
-      wrap = document.createElement('div'),
-      bar = wrap.appendChild(document.createElement('div')),
-
-      cursor = document.createElement('div'),
+    var onePass = options.onePass,
+      speedFactor = (1 / (options.speedFactor || 1)) * 165000,
+      wrap = document.createElement("div"),
+      bar = wrap.appendChild(document.createElement("div")),
+      cursor = document.createElement("div"),
       // If the user specified that the visibility is hidden, then we
       // start at the first pass ... otherwise we just do the
       // cursor fly-by
-      bodyVisibility = window.getComputedStyle(document.body, null).getPropertyValue('visibility'),
-      pass = (bodyVisibility === 'visible') ? 1 : 0,
+      bodyVisibility = window
+        .getComputedStyle(document.body, null)
+        .getPropertyValue("visibility"),
+      pass = bodyVisibility === "visible" ? 1 : 0,
       height = window.innerHeight,
       width = window.innerWidth,
-
       // this makes the loading of the screen proportional to the real-estate of the window.
       // it helps keep the cool sequence there while not making it waste too much time.
-      rounds = (height * width / speedFactor),
-      column = width, row = height - character.height;
+      rounds = (height * width) / speedFactor,
+      column = width,
+      row = height - character.height;
 
-    wrap.id = 'wrap386';
-    bar.id = 'bar386';
-    cursor.id = 'cursor386';
+    wrap.id = "wrap386";
+    bar.id = "bar386";
+    cursor.id = "cursor386";
 
     if (options.background) {
-      wrap.style.background = options.background
-      bar.style.background = options.background
+      wrap.style.background = options.background;
+      bar.style.background = options.background;
     }
 
     if (options.cursorColor) {
-      bar.style.color = options.cursorColor
-      cursor.style.color = options.cursorColor
+      bar.style.color = options.cursorColor;
+      cursor.style.color = options.cursorColor;
     }
 
-    cursor.innerHTML = bar.innerHTML = '&#9604;';
+    cursor.innerHTML = bar.innerHTML = "&#9604;";
 
     // only inject the wrap if the pass is 0
     if (pass === 0) {
       document.body.appendChild(wrap);
-      document.body.style.visibility = 'visible';
+      document.body.style.visibility = "visible";
     } else {
       document.body.appendChild(cursor);
       rounds /= 2;
@@ -80,14 +80,15 @@ export default function init386(options = {}) {
         }
 
         if (pass === 0) {
-          bar.style.width = column + 'px';
-          wrap.style.height = row + 'px';
+          bar.style.width = column + "px";
+          wrap.style.height = row + "px";
         } else {
-          cursor.style.right = column + 'px';
-          cursor.style.bottom = row + 'px';
+          cursor.style.right = column + "px";
+          cursor.style.bottom = row + "px";
         }
       }
     }, 1);
   }
+
   loading();
-}
+};
